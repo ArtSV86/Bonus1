68a16619929cade657279b498a593021a8ef97a4
Artur Sadov <Art.S.V.86@yandex.ru>
2021-03-29 22:38:37 +0400

    Added Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 5;
+        int cost = 3000;
+        int totalBonus = cost * bonus / 100;
+        System.out.println(totalBonus);
+    }
+}
